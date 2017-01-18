e04147bc2e0d8d857e4e7ceb15e43c64d50c7c6b
Brice Waegeneire <brice.wge@gmail.com>
2017-01-18 20:06:01 +0100

    update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/18 13:24:59 by bwaegene          #+#    #+#             */
-/*   Updated: 2017/01/18 13:35:38 by bwaegene         ###   ########.fr       */
+/*   Updated: 2017/01/18 20:04:14 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,18 @@
 int		main(int argc, char **argv)
 {
 	int		fd;
-	char	**line;
+	char	*line;
 
 	if (argc > 1)
 	{
-		line = NULL;
+		line = "\0";
 		fd = open(argv[1], O_RDONLY);
-		while (get_next_line(fd, line) > 0)
-			;
-		printf("%s", *line);
+		while (get_next_line(fd, &line) > 0)
+		{
+			ft_putchar('#');
+			ft_putstr(line);
+			line = "\0";
+		}
 		close (fd);
 	}
 	else

0d2bb96612b33900ebbc6f113bd796ac794755ff
Brice Waegeneire <brice.wge@gmail.com>
2017-01-21 14:04:33 +0100

    fix all the leaks, replace ft_strjoin by ft_strjoinf

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,17 @@
 /*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/18 11:44:31 by bwaegene          #+#    #+#             */
-/*   Updated: 2017/01/20 19:05:07 by bwaegene         ###   ########.fr       */
+/*   Updated: 2017/01/21 14:04:17 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
 
 void	store_rest(char *str, char **rest)
 {
 	if (*str)
 		*rest = ft_strdup(str);
 }
-
 
 int		get_next_line(const int fd, char **line)
 {
@@ -37,14 +35,14 @@ int		get_next_line(const int fd, char **line)
 		if ((end_line = ft_strchr(rest, '\n')))
 		{
 			*end_line = '\0';
-			*line = ft_strjoin(*line, rest);
+			*line = ft_strjoinf(*line, rest, 1);
 			store_rest(++end_line, &rest);
 			if (ft_strequ(rest, *line))
 				*rest = '\0';
 			return (1);
 		}
 		else
-			*line = ft_strjoin(*line, rest);
+			*line = ft_strjoinf(*line, rest, 1);
 		if (!ft_strchr(rest, '\n'))
 			*rest = '\0';
 	}
@@ -55,10 +53,10 @@ int		get_next_line(const int fd, char **line)
 		{
 			*end_line = '\0';
 			store_rest(++end_line, &rest);
-			*line = ft_strjoin(*line, buf);
+			*line = ft_strjoinf(*line, buf, 1);
 			return (1);
 		}
-		*line = ft_strjoin(*line, buf);
+		*line = ft_strjoinf(*line, buf, 1);
 	}
 	// On est arrive a la fin du fichier
 	if (**line && ret == 0)

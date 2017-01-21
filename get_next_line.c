dc0d88eb4db912c44b34b82ec52059294c8552db
Brice Waegeneire <brice.wge@gmail.com>
2017-01-21 14:46:13 +0100

    Merge form develop, ready to be turned in

---
+++ get_next_line.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/18 11:44:31 by bwaegene          #+#    #+#             */
+/*   Updated: 2017/01/21 14:14:21 by bwaegene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	store_rest(char *str, char **rest)
+{
+	if (*str)
+		*rest = ft_strdup(str);
+}
+
+int		use_rest(char **line, char **rest, char **end_line)
+{
+	if (**rest)
+	{
+		if ((*end_line = ft_strchr(*rest, '\n')))
+		{
+			**end_line = '\0';
+			*line = ft_strjoinf(*line, *rest, 1);
+			store_rest(++(*end_line), rest);
+			if (ft_strequ(*rest, *line))
+				**rest = '\0';
+			return (1);
+		}
+		else
+			*line = ft_strjoinf(*line, *rest, 1);
+		if (!ft_strchr(*rest, '\n'))
+			**rest = '\0';
+	}
+	return (0);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	int				ret;
+	char			*end_line;
+	char			buf[BUF_SIZE + 1];
+	static	char	*rest = "";
+
+	if (fd < 0 || !line)
+		return (-1);
+	*line = ft_strnew(0);
+	if (use_rest(line, &rest, &end_line))
+		return (1);
+	while ((ret = read(fd, buf, BUF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		if ((end_line = ft_strchr(buf, '\n')))
+		{
+			*end_line = '\0';
+			store_rest(++end_line, &rest);
+			*line = ft_strjoinf(*line, buf, 1);
+			return (1);
+		}
+		*line = ft_strjoinf(*line, buf, 1);
+	}
+	if (**line && ret == 0)
+		return (1);
+	return (ret);
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwaegene <bwaegene@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/18 11:33:27 by bwaegene          #+#    #+#             */
+/*   Updated: 2017/01/21 14:17:42 by bwaegene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUF_SIZE 32
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
+
+int		get_next_line(const int fd, char **line);
+#endif
